299020a8ca6d0d98717befdef2578de92a19153c
dezhizhang <1541609448@qq.com>
2025-07-08 07:27:49 +0800

    feat: 添加玩家左右移动

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,26 +2,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // 玩家物体
-    public GameObject player;
-    // 玩家移动的方向
-    public Vector2 playerDir;
+    [Header("PlayerMove")]
     // 玩家刚体
     public Rigidbody2D rb;
-    
-    void Update()
+    // 玩家按臽输入
+    private float _xInput;
+    private float _speed = 5;
+
+
+    private void Update()
     {
         PlayerMove();
     }
 
+
+    private void FixedUpdate()
+    {
+        FixPlayerMove();
+    }
+
     /// <summary>
-    /// 玩家移动
+    /// 玩家键盘输入
     /// </summary>
     private void PlayerMove()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(playerDir);
-        }
+        // 获取鼠标输入
+        _xInput = Input.GetAxisRaw("Horizontal");
+        Debug.Log(_xInput);
+    }
+
+
+    /// <summary>
+    /// 玩家移动
+    /// </summary>
+    private void FixPlayerMove()
+    {
+        rb.linearVelocity = new Vector2(_xInput * _speed, rb.linearVelocityY);
     }
 }
